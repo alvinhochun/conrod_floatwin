f928dda2daeecbdf68ea07862b529940628c1c7b
Alvin Wong <alvinhochun@gmail.com>
2020-08-16 17:05:01 +0800

    Impl. classic title bar buttons

--- src/classic_frame.rs
+++ src/classic_frame.rs
@@ -194,6 +194,42 @@ pub(super) fn make_panel_frame(
     make_frame(bottom_left, top_right, border_thickness, frame_color)
 }
 
+pub(super) fn make_button_frame(
+    bottom_left: [f64; 2],
+    top_right: [f64; 2],
+    border_thickness: f64,
+    base_color: color::Color,
+    is_clicked: bool,
+) -> impl Iterator<Item = widget::triangles::Triangle<widget::triangles::ColoredPoint>> {
+    let hsla = base_color.to_hsl();
+    let alpha = hsla.3;
+    // The original colors are greyscale with luminance of:
+    //     0.0, 1.0, 0.5, 0.875, 0.75
+    // We treat the base colour as the fifth colour and scale the other colours
+    // based on the original scales --
+    //     0.875 = (1.0 - 0.75) / 2.0 + 0.75
+    //     0.5 = 0.75 / 1.5
+    let lower_a = color::Rgba(0.0, 0.0, 0.0, alpha);
+    let upper_a = color::Rgba(1.0, 1.0, 1.0, alpha);
+    let lower_b = color::hsla(hsla.0, hsla.1, hsla.2 / 1.5, alpha).to_rgb();
+    let upper_b = color::hsla(hsla.0, hsla.1, (1.0 - hsla.2) / 2.0 + hsla.2, alpha).to_rgb();
+    let (lower_a, upper_a, lower_b, upper_b) = if is_clicked {
+        (upper_a, lower_a, upper_b, lower_b)
+    } else {
+        (lower_a, upper_a, lower_b, upper_b)
+    };
+    let inside = base_color.to_rgb();
+    let frame_color = FrameColor {
+        lower_a,
+        upper_a,
+        lower_b,
+        upper_b,
+        inside,
+    };
+
+    make_frame(bottom_left, top_right, border_thickness, frame_color)
+}
+
 pub(super) fn make_title_bar_gradient(
     bottom_left: [f64; 2],
     top_right: [f64; 2],
@@ -211,4 +247,141 @@ pub(super) fn make_title_bar_gradient(
         ([x_e, y_o], color_right),
     ])
     .map(widget::triangles::Triangle)
+}
+
+pub(super) fn make_close_button_icon(
+    bottom_left: [f64; 2],
+    top_right: [f64; 2],
+    hidpi_factor: f64,
+) -> impl Iterator<Item = widget::triangles::Triangle<conrod_core::Point>> {
+    let [x_o, y_o] = bottom_left;
+    let [x_e, y_e] = top_right;
+    let width = x_e - x_o;
+    let height = y_e - y_o;
+    let px_width = (width * hidpi_factor).round();
+    let px_height = (height * hidpi_factor).round();
+    let (icon_px_width, icon_px_height) = {
+        let expected_px_height = (px_width / 8.0 * 7.0).round();
+        if px_height < expected_px_height {
+            let expected_px_width = (px_height / 7.0 * 8.0).floor();
+            (expected_px_width, (expected_px_width / 8.0 * 7.0).floor())
+        } else {
+            ((expected_px_height / 7.0 * 8.0).round(), expected_px_height)
+        }
+    };
+    let sx = icon_px_width / 5.0;
+    let sy = sx - (icon_px_width - icon_px_height);
+
+    let icon_width = icon_px_width / hidpi_factor;
+    let icon_height = icon_px_height / hidpi_factor;
+    let sx = sx / hidpi_factor;
+    let sy = sy / hidpi_factor;
+    let x_mid_offset = (icon_width / 2.0) - sx;
+    let y_mid_offset = (icon_height / 2.0) - sy;
+
+    let part1 = polygon_to_triangle_points(value_iter_chain![
+        [x_o, y_o],
+        [x_o, y_o + sy],
+        [x_o + icon_width - sx, y_o + icon_height],
+        [x_o + icon_width, y_o + icon_height],
+        [x_o + icon_width, y_o + icon_height - sy],
+        [x_o + sx, y_o],
+    ]);
+    let part2 = polygon_to_triangle_points(value_iter_chain![
+        [x_o, y_o + icon_height],
+        [x_o + sx, y_o + icon_height],
+        [x_o + sx + x_mid_offset, y_o + icon_height - x_mid_offset],
+        [x_o + y_mid_offset, y_o + sy + y_mid_offset],
+        [x_o, y_o + icon_height - sy],
+    ]);
+    let part3 = polygon_to_triangle_points(value_iter_chain![
+        [x_o + icon_width, y_o],
+        [x_o + icon_width - sx, y_o],
+        [x_o + sx + x_mid_offset, y_o + x_mid_offset],
+        [x_o + icon_width - y_mid_offset, y_o + sy + y_mid_offset],
+        [x_o + icon_width, y_o + sy],
+    ]);
+    iter_chain![part1, part2, part3,].map(widget::triangles::Triangle)
+}
+
+pub(super) fn make_uncollapse_button_icon(
+    bottom_left: [f64; 2],
+    top_right: [f64; 2],
+    hidpi_factor: f64,
+) -> impl Iterator<Item = widget::triangles::Triangle<conrod_core::Point>> {
+    let [x_o, y_o] = bottom_left;
+    let [x_e, y_e] = top_right;
+    let width = x_e - x_o;
+    let height = y_e - y_o;
+    let px_width = (width * hidpi_factor).round();
+    let px_height = (height * hidpi_factor).round();
+    let (_icon_px_width, icon_px_height) = {
+        let expected_px_height = (px_width / 8.0 * 7.0).round();
+        if px_height < expected_px_height {
+            let expected_px_width = (px_height / 7.0 * 8.0).floor();
+            (expected_px_width, (expected_px_width / 8.0 * 7.0).floor())
+        } else {
+            ((expected_px_height / 7.0 * 8.0).round(), expected_px_height)
+        }
+    };
+    let half_height = icon_px_height / 2.0;
+    let pad_left = (half_height.round() / 2.0).round();
+    // let sx = half_height.ceil() - half_height;
+
+    let icon_height = icon_px_height / hidpi_factor;
+    let half_height = half_height / hidpi_factor;
+    let pad_left = pad_left / hidpi_factor;
+    // let sx = sx / hidpi_factor;
+
+    let triangle = [
+        [x_o + pad_left, y_o],
+        [x_o + pad_left, y_o + icon_height],
+        [x_o + pad_left + half_height, y_o + half_height],
+    ];
+    std::iter::once(triangle).map(widget::triangles::Triangle)
+    // let polygon = polygon_to_triangle_points(value_iter_chain![
+    //     [x_o + pad_left, y_o],
+    //     [x_o + pad_left, y_o + icon_height],
+    //     [x_o + pad_left + sx, y_o + icon_height],
+    //     [x_o + pad_left + sx + half_height, y_o + half_height],
+    //     [x_o + pad_left + sx, y_o],
+    // ]);
+    // polygon.map(widget::triangles::Triangle)
+}
+
+pub(super) fn make_collapse_button_icon(
+    bottom_left: [f64; 2],
+    top_right: [f64; 2],
+    hidpi_factor: f64,
+) -> impl Iterator<Item = widget::triangles::Triangle<conrod_core::Point>> {
+    let [x_o, y_o] = bottom_left;
+    let [x_e, y_e] = top_right;
+    let width = x_e - x_o;
+    let height = y_e - y_o;
+    let px_width = (width * hidpi_factor).round();
+    let px_height = (height * hidpi_factor).round();
+    let (_icon_px_width, icon_px_height) = {
+        let expected_px_height = (px_width / 8.0 * 7.0).round();
+        if px_height < expected_px_height {
+            let expected_px_width = (px_height / 7.0 * 8.0).floor();
+            (expected_px_width, (expected_px_width / 8.0 * 7.0).floor())
+        } else {
+            ((expected_px_height / 7.0 * 8.0).round(), expected_px_height)
+        }
+    };
+    let half_height = icon_px_height / 2.0;
+    let actual_width = icon_px_height;
+    let pad_top = (half_height.round() / 2.0).round();
+
+    let icon_height = icon_px_height / hidpi_factor;
+    let icon_width = actual_width / hidpi_factor;
+    let half_height = half_height / hidpi_factor;
+    let pad_top = pad_top / hidpi_factor;
+
+    let triangle = [
+        [x_o, y_o + icon_height - pad_top],
+        [x_o + icon_width, y_o + icon_height - pad_top],
+        [x_o + half_height, y_o + icon_height - pad_top - half_height],
+    ];
+    std::iter::once(triangle).map(widget::triangles::Triangle)
 }
