0f915406f7ae7146319f73de739c38b974f31e43
Alvin Wong <alvinhochun@gmail.com>
2020-08-19 10:20:12 +0800

    Add re-exports in crate root

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ mod classic_button;
 mod classic_frame;
 mod empty_widget;
 mod util;
+
+pub use windowing_area::{
+    layout::{WinId, WindowingState},
+    WindowBuilder, WindowEvent, WindowSetter, WindowingArea, WindowingContext,
+};
